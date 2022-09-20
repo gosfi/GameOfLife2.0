2f439859ba41c87f880530ced337ebe9c3ac42f1
Samuel Lefrançois <44752770+gosfi@users.noreply.github.com>
2022-09-19 20:54:41 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        byte[,] PlainBoard(int width, int height) 
+        {
+            byte[,] board = new byte[width, height];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i,j] = 0;
+                }
+            }
+
+            return board;
+        }
+    }
+}
